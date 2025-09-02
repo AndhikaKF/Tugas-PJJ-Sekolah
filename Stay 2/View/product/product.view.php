e01ceeb50f2ed13260f598cb3c654776bd1ce13d
Andhika <andhikafahmi41@gmail.com>
2025-09-02 10:11:19 +0700

    Add files via upload

---
+++ Stay 2/View/product/product.view.php
@@ -0,0 +1,32 @@
+<?php
+class ProductView extends productController
+{
+    public function show()
+    {
+        $products = $this->getProducts(); // call func controller product
+        foreach ($products as $product) { ?>
+            <tr>
+                <td><?php echo $product['id']; ?></td>
+                <td><?php echo $product['name']; ?></td>
+                <td><?php echo $product['price']; ?></td>
+                <td><?php echo $product['stock']; ?></td>
+            </tr>
+        <?php }
+    }
+
+    public function find()
+    {
+        $detailProduct = $this->getProductBy(); // call func controller product
+        if (empty($detailProduct))
+            return;
+
+        foreach ($detailProduct as $detail) { ?>
+            <div>
+                <h3><?php echo $detail['name']; ?></h3>
+                <p>Harga: Rp. <?php echo $detail['price']; ?></p>
+                <p>Stok: <?php echo $detail['stock']; ?></p>
+            </div>
+        <?php }
+    }
+}
+?>
---
+++ Stay 2/connection.php
@@ -0,0 +1,19 @@
+<?php
+class Connection
+{
+    private $hostname = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $dbname   = "stayern";
+
+    public function connect()
+    {
+        return new mysqli(
+            $this->hostname,
+            $this->username,
+            $this->password,
+            $this->dbname
+        );
+    }
+}
+?>
